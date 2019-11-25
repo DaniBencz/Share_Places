f73217ad0b6e66d7bd41cd5069023fd77c7ee024
Daniel <daniel.bencz@gmail.com>
2019-11-25 13:48:39 +0100

    trying map

--- ios/SharePlaces/AppDelegate.m
+++ ios/SharePlaces/AppDelegate.m
@@ -10,11 +10,13 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBG9yxDdlFiWtVqHy-vkIOvHuB4qpnWwAA"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"SharePlaces"
